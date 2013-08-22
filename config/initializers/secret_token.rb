c1293b364d4508e2d43e8ef558c32ca32d22c206
test <test@test.test>
2013-08-22 15:28:43 +0700

    firat commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Web::Application.config.secret_key_base = 'c0707051bc230ab905c5ab7e6dbb6c1ae2c02cafafa693ee4f87327da8a6f20112fba61df1a53d1ee200e7fc486ae22dbdd5851ff2fbba60ec1a6b76a6270476'
